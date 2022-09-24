0dc1bc19ed291fa5dc7ae8f254f22f5c9041ae17
Do-Tommy <55312117+Do-Tommy@users.noreply.github.com>
2022-09-24 00:57:53 -0700

    website redesign with modal support

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ function removeAllChildNodes(parent) {
   var flavor_list = "strawberry mango peach passionfruit blackberry tanerine cherry plum kiwi blueberry litchi watermelon greenapple guava banana coconut lemon ginger honey pineapple raspberry pomegranate rose lavendar chocolate vanilla cinammon chocolate cookiedough marshamallow saltedcaramel hazelnut almond gingebread caramel pumpkinspice almondroca taro match redbean honeydew"
   flavor_list = flavor_list.split(" ");
   var toppings = ["boba", "poppingboba", "redbean", "taro", "bobabran"];
-//   var flavorli = document.getElementById("flavor_list");
-//   for (var i = 0; i < flavor_list.length; i++ ) {
-//     var item = document.createElement("li");
-//     item.appendChild(document.createTextNode(flavor_list[i]));
-//     flavorli.appendChild(item);
-// }
-  
+
   var flavors = [];
   var topping_list = [];
   function getFlavors() {
@@ -46,15 +40,21 @@ function removeAllChildNodes(parent) {
     
     var ul = document.getElementById("results");
     var li = document.createElement("li");
+
     li.className = "list-group-item text-primary";
+    li.id = "flavor1";
     li.appendChild(document.createTextNode(flavors[0]));
     ul.appendChild(li);
+
     li = document.createElement("li");
     li.className = "list-group-item text-primary";
+    li.id = "flavor2";
     li.appendChild(document.createTextNode(flavors[1]));
     ul.appendChild(li);
+
     li = document.createElement("li");
     li.className = "list-group-item text-primary";
+    li.id = "flavor3";
     li.appendChild(document.createTextNode(flavors[2]));
     ul.appendChild(li);
 
@@ -66,23 +66,48 @@ function removeAllChildNodes(parent) {
       item.appendChild(document.createTextNode(topping_list[x]));
       topli.appendChild(item);
     }
+
     var milk_choice = Math.random() < 0.5 ? 'yes' : 'no' ;
     console.log(milk_choice);
     var milk = document.getElementById("milk");
     milk.className = "text-danger"
     milk.innerHTML= "your milk is " + milk_choice;
 
+    var flavor1 = document.getElementById("flavor1");
+    flavor1.addEventListener("click", () => {
+      console.log(flavor1.innerHTML);
+    })
 
-    // var text = document.createTextNode(flavors);
-    // flavorHtml.appendChild(text);
-    // resultDiv.appendChild(flavorHtml);
-    // text = document.createTextNode(topping_list);
-    // flavorLine.appendChild(text);
-    // resultDiv.appendChild(flavorLine);
+    var flavor2 = document.getElementById("flavor2");
+    flavor2.addEventListener("click", () => {
+      console.log(flavor2.innerHTML);
+    })
+
+    var flavor3 = document.getElementById("flavor3");
+    flavor3.addEventListener("click", () => {
+      console.log(flavor3.innerHTML);
+    })
+
+    
+    
   
     flavors = [];
     topping_list = [];
   }
+  function openModal() {
+    var modal = document.getElementById("flavormodal");
+    modal.style.display = "block";
+  }
+  function closeModal() {
+    var modal = document.getElementById("flavormodal");
+    modal.style.display = "none";
+  }
+
+
+
   
   document.getElementById("rand_button").addEventListener("click", getFlavors);
   
+
+ document.getElementById("navFlavorModal").addEventListener("click",openModal);
+document.getElementsByClassName("close")[0].addEventListener("click",closeModal);
